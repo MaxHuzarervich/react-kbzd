8c84ceae68d5c5b8076194dfac5fc5c6ed9866a2
MaxHuzarevich <pinsk.maks@mail.ru>
2021-04-27 10:50:25 +0300

    “stars-and-accord

---
+++ my-app/src/App.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import './App.css';
+import Accordion from "./components/Accordion/Accordion";
+import {Rating} from "./components/Rating/Rating";
+import OnOff from "./components/OnOff/OnOff";
+import UncontrolledAccordion from "./components/UncontrolledAccordion/Accordion";
+import {UncontrolledRating} from "./components/UncontrolledRating/Rating";
+
+function App() {
+    console.log('App rendering')
+    return (
+        <div className={'App'}>
+
+            <OnOff  />
+            {/*<OnOff  />*/}
+            {/*<OnOff  />*/}
+            {/*<OnOff  />*/}
+
+            <UncontrolledAccordion titleValue={'Menu'} collapsed={true}/>
+            {/*<UncontrolledAccordion titleValue={'Users'} collapsed={false}/>*/}
+
+            <UncontrolledRating />
+
+            {/*<OnOff on={false} />*/}
+            {/*<OnOff on={false} />*/}
+
+            {/*<Accordion titleValue={'Menu'} collapsed={true}/>*/}
+            {/*<Accordion titleValue={'Users'} collapsed={false}/>*/}
+
+            {/*<Rating value={0}/>*/}
+            {/*<Rating value={1}/>*/}
+            {/*<Rating value={2}/>*/}
+            {/*<Rating value={3}/>*/}
+            {/*<Rating value={4}/>*/}
+            {/*<Rating value={5}/>*/}
+
+        </div>
+    );
+}
+
+type PageTitlePropsType = {
+    title: string
+}
+
+function PageTitle(props: PageTitlePropsType) {
+    debugger
+    // console.log('PageTitle rendering')
+    return <h1> {props.title} </h1>
+}
+
+export default App;
