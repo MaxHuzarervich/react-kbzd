8b21b81bb58163afcb5d55ffd09315c69f3f0e56
MaxHuzarevich <pinsk.maks@mail.ru>
2021-06-24 17:39:29 +0300

    1) onChange 2) useRef

--- my-app/src/stories/assets/input.stories.tsx
+++ my-app/src/stories/assets/input.stories.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {ChangeEvent, useRef, useState} from 'react';
 
 export default {
   title: 'input',
@@ -7,10 +7,25 @@ export default {
 export const UncontrolledInput = () => <input/>;
 export const TrackValueOfUncontrolledInput = () => {
   const [value,setValue] = useState('');
-  return <><input onChange={(e) => {
-  const actualValue = e.currentTarget.value;
-  setValue(actualValue);
-  }}/> - {value} </>;
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const actualValue = e.currentTarget.value;
+    setValue(actualValue);
+  }
+
+  return <><input onChange={onChange}/> - {value} </>;
+}
+//-----------------------------------------------------------------------
+export const GetValueOfUncontrolledInputByButtonPress = () => {
+  const [value,setValue] = useState('');
+  //с помощью хука useRef создаем ссылку
+  const inputRef = useRef<HTMLInputElement>(null);
+  //в коллбеке к этой ссылке обращаемся
+  const save = ()=>
+  {const el = inputRef.current as HTMLInputElement;
+    setValue(el.value)}
+ //далее эту ссылку привязываем чтобы на нее ссылаться
+  return <><input ref={inputRef}/> <button onClick={save}>save</button> - actual value: {value} </>;
 }
 
 
