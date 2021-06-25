ba27c70fdb331fb3957726bd25064bcf5681e70c
MaxHuzarevich <pinsk.maks@mail.ru>
2021-06-25 17:52:26 +0300

    15 learn toggleItems

--- my-app/src/components/Select/Select.stories.tsx
+++ my-app/src/components/Select/Select.stories.tsx
@@ -2,10 +2,26 @@ import React from "react";
 import {Select} from "./Select";
 import {action} from "@storybook/addon-actions";
 
- export default {
+export default {
     title: 'Select',
     component: Select
 };
-                                   //action из сторибука выведет в консоль 'value changed'
-                                  // если вдруг коллбек будет вызван внутри компоненты select
-export const EmptyRating = () => <Select onChange={action('value changed')}/>
+//action из сторибука выведет в консоль 'value changed'
+// если вдруг коллбек будет вызван внутри компоненты select
+export const WithValue = () =>
+    <>
+        <Select onChange={action('value changed')}
+                value={'2'}
+                items={[
+                    {value: '1', title: 'Minsk'},
+                    {value: '2', title: 'Moscow'},
+                    {value: '3', title: 'Kiev'}]}/>
+    </>
+export const WithoutValue = () =>
+    <>
+        <Select onChange={action('value changed')}
+                items={[
+                    {value: '1', title: 'Minsk'},
+                    {value: '2', title: 'Moscow'},
+                    {value: '3', title: 'Kiev'}]}/>
+    </>
--- my-app/src/components/Select/Select.tsx
+++ my-app/src/components/Select/Select.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, {useState} from "react";
+import styles from './Select.module.css';
 
 type ItemType = {
     title: string
@@ -6,15 +7,37 @@ type ItemType = {
 }
 
 type SelectPropsType = {
-    value: any,
+    value?: any,
     onChange: (value: any) => void
     items: ItemType []  // массив строк!
 };
 
 export function Select(props: SelectPropsType) {
-    console.log('Select rendering')
 
-    return (<div>
-        <div>{}</div>
-        {props.items.map(i => <div>{i.title}</div>)}
-    </div>)}
+    const [active, setActive] = useState(false)
+
+    //находим тот items value которого равно тому value которое нам передали в пропсах
+    const selectedItem = props.items.find(i => i.value === props.value)
+
+    const toggleItems = () => setActive(!active) //противоположное значение active
+
+    return (
+        <>
+            <select>
+                <option value={''}>Minsk</option>
+                <option value={''}>Moscow</option>
+                <option value={''}>Kiev</option>
+            </select>
+
+            <div className={styles.select}>
+                <h3 onClick={toggleItems}>{selectedItem && selectedItem.title}</h3>
+                { //если класс active то рисуем эту дивку
+                    active &&
+                    <div className={styles.items}>
+                        {props.items.map(i => <div key={i.value}>{i.title}</div>)}
+                    </div>
+                }
+            </div>
+        </>
+    )
+}
