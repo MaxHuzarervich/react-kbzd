6e06bb92c2d52ef5cda0fa90f7e0383a84bf91af
MaxHuzarevich <pinsk.maks@mail.ru>
2021-06-27 14:35:09 +0300

    17 factorial

--- my-app/src/stories/UseMemo.stories.tsx
+++ my-app/src/stories/UseMemo.stories.tsx
@@ -6,13 +6,30 @@ export default {
 
 export const Example1 = () => {
 
-const [a, setA] = useState<number>(0)
-const [b, setB] = useState<number>(0)
+    const [a, setA] = useState<number>(3)
+    const [b, setB] = useState<number>(3)
+
+    let resultA = 1;
+    let resultB = 1;
+
+    for (let i = 1; i <= a; i++) {    //расчет факториала числа a
+        resultA = resultA * i;
+    }
+    for (let i = 1; i <= b; i++) {    //расчет факториала числа b
+        resultB = resultB * i;
+    }
 
     return <>
-    <input value={a}
-           onChange={(e) => setA(Number(e.currentTarget.value))}/>
-    <input value={b}   //приведение типа ( + и Number одно и то же )
-           onChange={(e) => setB(+e.currentTarget.value)}/>
+        <input value={a}
+               onChange={(e) => setA(Number(e.currentTarget.value))}/>
+        <input value={b}   //приведение типа ( + и Number одно и то же )
+               onChange={(e) => setB(+e.currentTarget.value)}/>
+        <hr/>
+        <div>
+            Result for a:
+        </div>
+        <div>
+            Result for b:
+        </div>
     </>
 }
