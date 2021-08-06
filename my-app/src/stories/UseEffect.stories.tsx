485ee3eade91eeedf47c7a79ee1e5262d33d6a9f
MaxHuzarevich <pinsk.maks@mail.ru>
2021-08-06 23:01:59 +0300

    22 useEffect Clock component

--- my-app/src/stories/UseEffect.stories.tsx
+++ my-app/src/stories/UseEffect.stories.tsx
@@ -5,55 +5,57 @@ export default {
 }
 
 
-export const SimpleExample = () => {
-
-const [fake, setFake] = useState(1);
-const [counter, setCounter] = useState(1);
-
-console.log('SimpleExample')
-
-    useEffect(() => {
-        console.log('useEffect first render and every change counter')
-        document.title = counter.toString()
-    },[counter]) //counter наша зависимость если не поменяется то и useEffect не будет перерисовываться
-
-    useEffect(() => {
-        console.log('useEffect only first render')
-        document.title = counter.toString()
-    },[])
-
-    useEffect(() => {
-        console.log('useEffect every render')
-        document.title = counter.toString()
-    })
-
-    return <>
-        Hello, {counter} {fake}
-        <button onClick={() => { setFake( fake + 1 ) }}> fake </button>
-        <button onClick={() => { setCounter( counter + 1 ) }}>counter +</button>
-    </>
-}
+// export const SimpleExample = () => {
+//
+// const [fake, setFake] = useState(1);
+// const [counter, setCounter] = useState(1);
+//
+// console.log('SimpleExample')
+//
+//     useEffect(() => {
+//         console.log('useEffect first render and every change counter')
+//         document.title = counter.toString()
+//     },[counter]) //counter наша зависимость если не поменяется то и useEffect не будет перерисовываться
+//
+//     useEffect(() => {
+//         console.log('useEffect only first render')
+//         document.title = counter.toString()
+//     },[])
+//
+//     useEffect(() => {
+//         console.log('useEffect every render')
+//         document.title = counter.toString()
+//     })
+//
+//     return <>
+//         Hello, {counter} {fake}
+//         <button onClick={() => { setFake( fake + 1 ) }}> fake </button>
+//         <button onClick={() => { setCounter( counter + 1 ) }}>counter +</button>
+//     </>
+// }
 
 export const SetTimeoutExample = () => {
 
     const [fake, setFake] = useState(1);
     const [counter, setCounter] = useState(1);
 
-    console.log('SetTimeoutExample')
+    console.log('SetIntervalExample')
 
-    // useEffect(() => {
-    //     console.log('useEffect first render and every change counter')
-    //     document.title = counter.toString()
-    // })
+    useEffect(() => {
 
-    setTimeout(() => {
-        debugger
-        document.title = counter.toString()
-    }, 1000)
+        // setInterval(() => {
+        //
+        //         console.log('tick :' + counter)
+        //
+        //     setCounter((state) => state + 1)
+        //     }
+        // , 1000)
+    }, [])
+
 
     return <>
-        Hello, {counter} {fake}
-        <button onClick={() => { setFake( fake + 1 ) }}> fake </button>
-        <button onClick={() => { setCounter( counter + 1 ) }}>counter +</button>
+        Hello, counter: {counter} - fake: {fake}
+        {/*<button onClick={() => { setFake( fake + 1 ) }}> fake </button>*/}
+        {/*<button onClick={() => { setCounter( counter + 1 ) }}>counter +</button>*/}
     </>
 }
